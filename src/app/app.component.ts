312f012a195ca566140860ddbe38fbcf1b6cd8ae
Alemazz99 <alessandro.mazzieri@studenti.unicam.it>
2023-05-17 20:24:14 +0200

    implemt ui & add item

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'todo-list';
+  title = "todo";
+
+  filter: "all" | "active" | "done" = "all";
+
+  allItems = [
+    { description: "Pane", done: true },
+  ];
+
+  get items() {
+    if (this.filter === "all") {
+      return this.allItems;
+    }
+    return this.allItems.filter((item) =>
+      this.filter === "done" ? item.done : !item.done
+    );
+  }
+  addItem(description: string) {
+    this.allItems.unshift({
+      description,
+      done: false
+    });
+  }
+  
 }
+
